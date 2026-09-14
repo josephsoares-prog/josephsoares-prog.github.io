4b2c94927fcc624f128bb639bcbc04b263f8d6c8
josephsoares-prog <joseph.soares@gmail.com>
2026-09-14 10:48:20 -0400

    consent.js: call wrapWideTables() from run()

--- consent.js
+++ consent.js
@@ -261,6 +261,7 @@
     if (navlinks) { navCSS(); navlinks.innerHTML = (IS_FR ? NAV_HTML_FR : NAV_HTML) + toggleHTML(); }
 
     mountMobileToggle();
+    wrapWideTables();
 
     var footers = document.querySelectorAll("footer");
     var footer = footers.length ? footers[footers.length - 1] : null;
